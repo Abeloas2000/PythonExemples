d250a6d4760f1fde2c1955088a01583ad0804c36
Abeloas2000 <54040519+Abeloas2000@users.noreply.github.com>
2020-10-13 22:21:10 -0300

    Create Exemple01.py

---
+++ Exemple01.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Digite aqui o valor que desejar:"))
+valor2 = int(input("Digite aqui um segundo valor:"))
+if valor1 < 300:
+  Print("O primeiro valor e o maior")
+else:
+  Print(" O segundo valor e o maior")
+  
